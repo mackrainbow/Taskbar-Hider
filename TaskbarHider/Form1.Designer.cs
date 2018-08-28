22821adab99b6834384d8b8479646dfad7dce24e
macrainbow <macintoshrainbow@gmail.com>
2018-08-28 19:25:26 +0500

    1.0

---
+++ TaskbarHider/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TaskbarHider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 79);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hide taskbar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 79);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show taskbar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 103);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Taskbar Hider";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TaskbarHider/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Security;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Windows.Automation;
+
+namespace TaskbarHider
+{
+    public partial class Form1 : Form
+    {
+
+        public static string processName =
+        /*========================== CONFIGURE THIS ==========================*/
+        /**/                            "notepad"                           /**/
+        /*========================== CONFIGURE THIS ==========================*/;
+
+        delegate void WinEventDelegate(
+            IntPtr hWinEventHook,
+            uint eventType,
+            IntPtr hwnd,
+            int idObject,
+            int idChild,
+            uint dwEventThread,
+            uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(
+            uint eventMin,
+            uint eventMax,
+            IntPtr hmodWinEventProc,
+            WinEventDelegate lpfnWinEventProc,
+            uint idProcess,
+            uint idThread,
+            uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        const uint EVENT_SYSTEM_FOREGROUND = 3;
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+
+        // Need to ensure delegate is not collected while we're using it,
+        // storing it in a class field is simplest way to do this.
+        static WinEventDelegate procDelegate = new WinEventDelegate(WinEventProc);
+
+        static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            // filter out non-HWND namechanges... (eg. items within a listbox)
+            if (idObject != 0 || idChild != 0)
+            {
+                return;
+            }
+
+            if (GetForegroundProcessName() == processName)
+                HideTaskbar();
+            else ShowTaskbar();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IntPtr hook = SetWinEventHook(
+                EVENT_SYSTEM_FOREGROUND,
+                EVENT_SYSTEM_FOREGROUND, 
+                IntPtr.Zero,
+                procDelegate, 
+                0, 
+                0, 
+                WINEVENT_OUTOFCONTEXT);
+
+            // MessageBox provides the necessary mesage loop that SetWinEventHook requires.
+            // In real-world code, use a regular message loop (GetMessage/TranslateMessage/
+            // DispatchMessage etc or equivalent.)
+            MessageBox.Show("Tracking HWNDs, close message box to exit.");
+
+            UnhookWinEvent(hook);
+        }
+
+        static void stopWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process stopped: {0}", e.NewEvent.Properties["ProcessName"].Value);
+            if (e.NewEvent.Properties["ProcessName"].Value.ToString() == "notepad.exe") ;
+        }
+
+        static void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Process started: {0}", e.NewEvent.Properties["ProcessName"].Value);
+            if (e.NewEvent.Properties["ProcessName"].Value.ToString() == "notepad.exe") ;
+        }
+
+        // The GetForegroundWindow function returns a handle to the foreground window
+        // (the window  with which the user is currently working).
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        // The GetWindowThreadProcessId function retrieves the identifier of the thread
+        // that created the specified window and, optionally, the identifier of the
+        // process that created the window.
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern Int32 GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        static private string GetForegroundProcessName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+
+            // The foreground window can be NULL in certain circumstances, 
+            // such as when a window is losing activation.
+            if (hwnd == null)
+                return "Unknown";
+
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (p.Id == pid)
+                    return p.ProcessName;
+            }
+
+            return "Unknown";
+        }
+
+        static public void ShowTaskbar()
+        {
+            Taskbar.Show();
+            SetTaskbarState(AppBarStates.AlwaysOnTop);
+        }
+
+        static public void HideTaskbar()
+        {
+            SetTaskbarState(AppBarStates.AutoHide);
+            Taskbar.Hide();
+            System.Threading.Thread.Sleep(100);
+            Taskbar.Hide();
+        }
+
+        //Getting process window-------------------------
+        private static WINDOWPLACEMENT GetPlacement(IntPtr hwnd)
+        {
+            WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+            placement.length = Marshal.SizeOf(placement);
+            GetWindowPlacement(hwnd, ref placement);
+            return placement;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetWindowPlacement(
+            IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public ShowWindowCommands showCmd;
+            public Point ptMinPosition;
+            public Point ptMaxPosition;
+            public Rectangle rcNormalPosition;
+        }
+
+        internal enum ShowWindowCommands : int
+        {
+            Hide = 0,
+            Normal = 1,
+            Minimized = 2,
+            Maximized = 3,
+        }
+        //-----------------------------------------------
+
+        //Hide taskbar-----------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetTaskbarState(AppBarStates.AutoHide);
+            Taskbar.Hide();
+            System.Threading.Thread.Sleep(100);
+            Taskbar.Hide();
+        }
+        //-----------------------------------------------
+
+        //Show taskbar-----------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Taskbar.Show();
+            SetTaskbarState(AppBarStates.AlwaysOnTop);
+        }
+        //-----------------------------------------------
+
+        //Set taskbar mode (on top/auto-hide)------------
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("shell32.dll")]
+        public static extern UInt32 SHAppBarMessage(UInt32 dwMessage, ref APPBARDATA pData);
+
+        public enum AppBarMessages
+        {
+            New = 0x00,
+            Remove = 0x01,
+            QueryPos = 0x02,
+            SetPos = 0x03,
+            GetState = 0x04,
+            GetTaskBarPos = 0x05,
+            Activate = 0x06,
+            GetAutoHideBar = 0x07,
+            SetAutoHideBar = 0x08,
+            WindowPosChanged = 0x09,
+            SetState = 0x0a
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct APPBARDATA
+        {
+            public UInt32 cbSize;
+            public IntPtr hWnd;
+            public UInt32 uCallbackMessage;
+            public UInt32 uEdge;
+            public Rectangle rc;
+            public Int32 lParam;
+        }
+
+        public enum AppBarStates
+        {
+            AutoHide = 0x01,
+            AlwaysOnTop = 0x02
+        }
+
+        static public void SetTaskbarState(AppBarStates option)
+        {
+            APPBARDATA msgData = new APPBARDATA();
+            msgData.cbSize = (UInt32)Marshal.SizeOf(msgData);
+            msgData.hWnd = FindWindow("System_TrayWnd", null);
+            msgData.lParam = (Int32)(option);
+            SHAppBarMessage((UInt32)AppBarMessages.SetState, ref msgData);
+        }
+
+        public AppBarStates GetTaskbarState()
+        {
+            APPBARDATA msgData = new APPBARDATA();
+            msgData.cbSize = (UInt32)Marshal.SizeOf(msgData);
+            msgData.hWnd = FindWindow("System_TrayWnd", null);
+            return (AppBarStates)SHAppBarMessage((UInt32)AppBarMessages.GetState, ref msgData);
+        }
+        //-----------------------------------------------
+    }
+}
